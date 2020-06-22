a8e4800dabfc232882a3afd996fa6290eaf12ce9
Guillaume Gagnaire <contact@scrola.net>
2020-06-22 22:49:54 +0200

    Video

--- pages/drummer.vue
+++ pages/drummer.vue
@@ -9,6 +9,8 @@
     <p>My favorite musical styles range from rock to death metal.</p>
 
     <p>Double pedal lovers, subscribe to my <a href="https://www.youtube.com/channel/UCeME3-22NI6fsq9tzT-jpOQ" target="_blank">Youtube channel</a>!</p>
+
+    <iframe width="560" height="315" style="margin: 42px auto 0 auto" src="https://www.youtube.com/embed/KQSrOuz9-Ck" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </article>
 </template>
 
