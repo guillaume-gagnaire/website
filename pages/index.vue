cb70667378b8e78e94aefb35e02349f72ac50719
Guillaume Gagnaire <contact@scrola.net>
2020-06-22 22:43:24 +0200

    SImple cv

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <nuxt-link to="/fullstack-developer" class="content d-flex flex-column text-center justify-content-center">
         <h2>Fullstack developer</h2>
         <p>
-          Developer since 2006, I'm currently CTO @ Noci.io.
+          Developer since 2006, I'm currently <b>CTO</b> @ <b>Noci.io</b>, in <b>Bordeaux, France</b>.
         </p>
       </nuxt-link>
       <div class="bg" style="background-image:url(/me.jpg)"></div>
@@ -22,7 +22,6 @@
 </template>
 
 <script>
-
 export default {
   components: {
   }
@@ -30,6 +29,9 @@ export default {
 </script>
 
 <style lang="scss">
+  $homeGutter: 5px;
+  $gutterColor: #000;
+
   .home {
     height: 100%;
     height: 100vh;
@@ -39,6 +41,14 @@ export default {
       width: 100%;
       height: 100%;
       position: relative;
+
+      &:first-child {
+        border-bottom: $homeGutter solid $gutterColor;
+      }
+
+      &:last-child {
+        border-top: $homeGutter solid $gutterColor;
+      }
 
       .bg {
         position: absolute;
@@ -65,9 +75,27 @@ export default {
         -ms-transition: background 400ms ease-out;
         -o-transition: background 400ms ease-out;
         transition: background 400ms ease-out;
+        padding: 20px;
 
         &:hover {
           background: rgba(0, 0, 0, 0.5);
+        }
+      }
+    }
+  }
+
+  
+  @media (min-width: 992px) { 
+    .home {
+      .panel {
+        &:first-child {
+          border-bottom: none;
+          border-right: $homeGutter solid $gutterColor;
+        }
+
+        &:last-child {
+          border-top: none;
+          border-left: $homeGutter solid $gutterColor;
         }
       }
     }
