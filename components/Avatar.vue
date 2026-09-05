ecce280a5f40dba716f352a272e7c10f336c2c4c
Guillaume Gagnaire <guillaume@gagnaire.dev>
2026-09-05 16:38:00 +0200

    Change PP

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -1,13 +1,18 @@
 <template>
+  <!--
+    La photo est un JPEG opaque : contrairement à l'ancienne PNG détourée,
+    elle ne peut pas être composée sur le dégradé de la charte. On la rend
+    donc plein cadre, le fond studio faisant office de fond de bloc.
+  -->
   <div
-    class="avatar bg-primary-900/5 dark:bg-primary-100/5 w-full max-w-[400px] h-[420px] mx-auto rounded-xl lg:-my-4 p-4"
-  ></div>
+    class="w-full max-w-[400px] h-[420px] mx-auto rounded-xl lg:-my-4 overflow-hidden"
+  >
+    <img
+      src="/me.jpg"
+      alt="Guillaume Gagnaire"
+      width="400"
+      height="420"
+      class="w-full h-full object-cover"
+    />
+  </div>
 </template>
-
-<style scoped>
-.avatar {
-  background: url(/me.png) no-repeat bottom center,
-    linear-gradient(180deg, #ff6f47 0%, #ff9447 100%);
-  background-size: contain;
-}
-</style>
